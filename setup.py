bfbd78995df3133f117bffe5769c81197d2800df
belle <belle.l24358@gmail.com>
2023-04-01 22:19:41 +0000

    Add submodule handytools

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="main",
+    version="0.0.0",
+    author="Belle Liu",
+    author_email="belle.l24358@gmail.com",
+    description='',
+    packages=find_packages(exclude=[]),
+)
+
+setup(
+    name="support",
+    version="0.0.0",
+    author="Belle Liu",
+    author_email="belle.l24358@gmail.com",
+    description='',
+    packages=find_packages(exclude=[]),
+)
+
+setup(
+    name="handytools",
+    version="0.0.0",
+    author="Belle Liu",
+    author_email="belle.l24358@gmail.com",
+    description='',
+    packages=find_packages(exclude=[]),
+)
