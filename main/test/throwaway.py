f12728a6f99e98ea27bae8c87449bf7e3ec27130
belle <belle.l24358@gmail.com>
2023-04-03 01:52:24 +0000

    Fit APC (done).

---
+++ main/test/throwaway.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import handytools.navigator as nav
+from spectools.metrics.metrics import fvmax, cubic_spline
+
+shape_coor = nav.pklload("/src", "data", "stimulus", "shape_coor.pkl")
+ps, dps, ddps = cubic_spline(np.array(shape_coor[0]).reshape(-1, 2))
+
+#================
+
+from scipy.interpolate import CubicSpline
+
+def cubic_spline2(invec, ival=1/200): # invec.shape = (-1, 2)
+    cs = CubicSpline(np.linspace(0, 1, len(invec)), invec)
+    xs = np.arange(0, 1, ival)
+    return cs(xs), cs(xs, 1), cs(xs, 2)
+
+ps, dps, ddps = cubic_spline2(np.array(shape_coor[0]).reshape(-1, 2))
+
+import pdb; pdb.set_trace()
--- spectools/metrics/metrics.py
+++ spectools/metrics/metrics.py
@@ -1,9 +1,34 @@
 import numpy as np
+from scipy.interpolate import CubicSpline
 
-def double_exp(x, mu_c, sig_c, mu_a, sig_a):
-    return np.exp(-(x - mu_c)**2/(2*sig_c**2)) * np.exp(-(x - mu_a)**2/(2*sig_a**2))
+def double_exp(ksts, coefs):
+    mu_c, sig_c, mu_a, sig_a, k = coefs
+
+    res = []
+    for x in ksts:
+        x_c, x_a = x.T
+        prob = np.exp(-(x_c - mu_c)**2/(2*sig_c**2)) * np.exp(-(x_a - mu_a)**2/(2*sig_a**2))
+        res.append(prob)
+    
+    return k*max(res)
+
+def double_exp_wrap(xbundle, coefs):
+    return np.array([double_exp(ksts, coefs) for ksts in xbundle])
+
+def cubic_spline(invec, ival=1/200): # invec.shape = (-1, 2)
+    cs = CubicSpline(np.linspace(0, 1, len(invec)), invec)
+    xs = np.arange(0, 1, ival)
+    return cs(xs), cs(xs, 1), cs(xs, 2)
+
+def curvature(dp, ddp):
+    numerator = np.linalg.norm(np.cross(dp, ddp))
+    denominator = np.linalg.norm(dp)**3
+    return numerator/denominator
+
+def angle(p): return np.arctan2(*p[::-1])
 
 def fvmax(invec):
+    """By Taekjun Kim, Anitha Pasupathy, Wyeth Bair, April 23, 2020."""
     num = np.shape(invec)[0]
     inshft = np.vstack((invec[num-2,:],invec,invec[1,:]))
     ip = np.arange(0,1,1/50)
@@ -35,9 +60,14 @@ def fvmax(invec):
         dvtx[0,i*50:(i+1)*50] = np.sum(np.tile(bufvrt[:,0].reshape(4,1),(1,50))*dincr,axis=0)/6.0
         dvty[0,i*50:(i+1)*50] = np.sum(np.tile(bufvrt[:,1].reshape(4,1),(1,50))*dincr,axis=0)/6.0
     
+    # periodic boundaries
     vtx[0,(num-1)*50] = vtx[0,0]
     vty[0,(num-1)*50] = vty[0,0]
+    dvtx[0,(num-1)*50] = dvtx[0,0]
+    dvty[0,(num-1)*50] = dvty[0,0]
     
     outvec = np.transpose(np.vstack((vtx, vty)))
     doutvec = np.transpose(np.vstack((dvtx, dvty)))
     return outvec, doutvec
+
+
