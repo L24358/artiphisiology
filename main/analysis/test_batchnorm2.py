753546c282e76581c341bb40d80c813c7e63a94e
L24358 <belle.l24358@gmail.com>
2023-05-19 14:41:29 -0700

    Minor fix.

--- main/analysis/test_batchnorm2.py
+++ main/analysis/test_batchnorm2.py
@@ -11,12 +11,14 @@ d = 5
 input_tensor = torch.from_numpy(np.arange(d * d).reshape((1, 1, d, d))).float()
 input_tensor2 = [input_tensor, input_tensor*2, input_tensor*5]
 input_tensor2 = torch.vstack(input_tensor2).swapaxes(0, 1)
+input_tensor3 = torch.vstack([input_tensor, input_tensor*5])
 
 bn = nn.BatchNorm2d(1)
 bn2 = nn.BatchNorm2d(3)
 
 output1 = bn(input_tensor) # 1 batch, 1 channel (--> mean=0, std=1)
 output2 = bn2(input_tensor2) # 1 batch, 3 channels (--> independent across channels)
+output3 = bn(input_tensor3)
 
 # Conclusion: is normalizing across space, not channels!
 import pdb; pdb.set_trace()
